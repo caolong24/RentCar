50c48688d9851437404a7881e10ff82e375f541c
caolong24 <129348535+caolong24@users.noreply.github.com>
2023-05-05 17:36:57 +0700

    add module-resolver
    Update index.ts
    
    add module-resolver

--- babel.config.js
+++ babel.config.js
@@ -1,3 +1,44 @@
 module.exports = {
   presets: ['module:metro-react-native-babel-preset'],
+  plugins: [
+    [
+      'module-resolver',
+      {
+        root: ['./'],
+        extensions: [
+          '.ios.js',
+          '.android.js',
+          '.js',
+          '.jsx',
+          '.ts',
+          '.tsx',
+          '.json',
+        ],
+        alias: {
+          '@Screens': './app/screens',
+          '@Components': './app/components',
+          '@I18n': './app/i18n',
+          '@Models': './app/models',
+          '@Navigators': './app/navigators',
+          '@Api': './app/services/api',
+          '@Native': './app/services/native',
+          '@NativeModule': './app/native-module',
+          '@Source': './app/source',
+          '@Theme': './app/theme',
+          '@Utils': './app/utils',
+          '@Assets': './app/assets',
+          '@Assets/*': './app/assets/*',
+          '@Libs': './app/libs',
+          '@Hooks': './app/hooks',
+          '@Types': './app/types',
+          '@Constants': './app/constants',
+        },
+      },
+    ],
+  ],
+  env: {
+    production: {
+      plugins: ['transform-remove-console'],
+    },
+  },
 };
